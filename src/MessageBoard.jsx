89a33c72f105b7f301232890f5cfb7d53dff4cf0
Jason Anderson <jason.anderson@elisa.fi>
2020-08-15 10:48:19 -0700

    basic infScroll

--- src/MessageBoard.jsx
+++ src/MessageBoard.jsx
@@ -8,6 +8,7 @@ class MessageBoard extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit1 = this.handleSubmit1.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.addToList = this.addToList.bind(this);
     const name = JSON.parse(sessionStorage.getItem('tokens'));
     const u = name.userName;
     this.state = {
@@ -20,6 +21,7 @@ class MessageBoard extends React.Component {
     showMessagesList2: false,
     showMessagesList3: true,
     list: null,
+    scrollPageNumber: 0,
     };
   }
 
@@ -46,6 +48,12 @@ class MessageBoard extends React.Component {
     elmnt.scrollIntoView();
   }
 
+  addToList() {
+    let newPageNo = ++this.state.scrollPageNumber;
+    this.setState({scrollPageNumber: newPageNo});
+    this.getMessagesList();
+  }
+
   postClubMessage() {
     const name = JSON.parse(sessionStorage.getItem('tokens'));
     const u = name.userName;
@@ -70,19 +78,24 @@ class MessageBoard extends React.Component {
         const token = u +':' + p;
         const hash = btoa(token);
         const Basic = 'Basic ' + hash;
-        axios.get("http://localhost:8080/api/m/c?cId=" + this.props.clubId,
+        axios.get("http://localhost:8080/api/m/c?cId=" + this.props.clubId + "&pN=" + this.state.scrollPageNumber,
         {headers : { 'Authorization' : Basic }})
         .then((response) => {
          if (response.status === 200) {
+
+            if (this.state.list == null) { this.setState({ list: response.data  }) }
+            else {
+            this.setState({ list: this.state.list.concat(response.data)  }) };
+
           this.setState({
             isLoaded: true,
-            list: response.data,
+            //list: response.data,
             showMessagesList: !this.state.showMessagesList, // initial list rendering
             showMessagesList2: true, // stays true since response is '200' and there is content to pass down to child.
             showMessagesList3: !this.state.showMessagesList3,
             clubMessage: null,
           });
-          this.scrollToBottom();
+          if (this.state.scrollPageNumber == 0) { this.scrollToBottom(); };
           } // end if
                }).catch(error => {this.setState({ isLoaded: true, error,});
                });
@@ -95,11 +108,11 @@ class MessageBoard extends React.Component {
 
         { this.state.showMessagesList &&
         <div>
-        <MessagesList list={this.state.list} showMessagesList2={this.state.showMessagesList2}  />
+        <MessagesList list={this.state.list} showMessagesList2={this.state.showMessagesList2} addToList={this.addToList} />
         </div> }
         { this.state.showMessagesList3 &&
         <div>
-        <MessagesList list={this.state.list} showMessagesList2={this.state.showMessagesList2}  />
+        <MessagesList list={this.state.list} showMessagesList2={this.state.showMessagesList2} addToList={this.addToList} />
         </div> }
 
         { this.state.showMessagesList &&
--- src/MessagesList.jsx
+++ src/MessagesList.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 class MessagesList extends React.Component {
     constructor(props) {
         super(props);
+        //this.addToList = this.handleScroll.bind(this);
         this.state = {
             list: this.props.list, //
             messagesEnd: true,
         };
     }
+
+    handleScroll = (e) => {
+        const top = e.target.scrollTop == 0;
+        if (top) {
+            this.props.addToList();  };
+     }
+
 
    renderTableData() {
     const sortedList = this.state.list.sort((a,b) => (a.created > b.created) ? 1 : ((b.created > a.created) ? -1 : 0));
@@ -41,7 +49,7 @@ class MessagesList extends React.Component {
          </div> }
 
         { this.props.showMessagesList2 &&
-         <div class="messagesDiv">
+         <div class="messagesDiv" onScroll={this.handleScroll}>
          <div>
             <table>
                <tbody>
